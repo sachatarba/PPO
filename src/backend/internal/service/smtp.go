d860c714d60ddc2b80ca0ceda408d9c4e7b422dc
Alexander Tarba <sachatarba@rambler.ru>
2024-12-07 03:51:52 +0300

    fix

--- src/backend/internal/service/smtp.go
+++ src/backend/internal/service/smtp.go
@@ -20,7 +20,7 @@ func NewSmtpService(config *config.SmtpConfig) *SmtpService {
 func (s *SmtpService) SendMail(text string, receiver string) error {
 	message := []byte("From: " + s.config.FromAddres + "\r\n" +
 		"To: " + receiver + "\r\n" +
-		"Subject: Ваш код подтверждения авторизации\r\n" +
+		"Subject: Код для прохождения двухфакторной авторизации\r\n" +
 		"\r\n" +
 		text + "\r\n")
 
